3d212f16e7c7cd4143f6b91356d01c03bccf5114
Arko Mandal <arko.online24@gmail.com>
2020-03-11 15:06:05 +0530

    indentation corrected for chat bubbles

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const io = require('socket.io')(http);
 const cors = require('cors');
 const jwt = require('./jwt/jwt');
 const errorHandler = require('./jwt/error-handler');
+const socket = require('./sockets');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -21,38 +22,5 @@ app.use('/message', require('./routes/message.route'));
 
 http.listen(PORT, () => {
     console.log("Server is listening on port ", PORT);
-});
-
-//socket configuration
-let onlineUsers = [];
-io.on('connection', function (socket) {
-    socket.on('user name', (user) => {
-        const details = { profileName: user.profileName, sessionId: socket.id };
-        onlineUsers.push(details);
-        io.emit('connectedUsers', onlineUsers); //io.sockets.emit == io.emit
-    });
-
-    socket.on('disconnect', function () {
-        onlineUsers = onlineUsers.filter((user) => user.sessionId !== socket.id);
-        io.emit('connectedUsers', onlineUsers);
-        // socket.leave(group);
-    });
-
-    //personal chat
-    socket.on('chatting', function (message, sender, receiver) {
-        socket.to(receiver).emit('receiverPeer', message, socket.id, receiver);
-        socket.emit('senderPeer', message, socket.id, receiver);
-    });
-
-    //group chat
-    socket.on('assign users', function (group) {
-        socket.join(group);
-    });
-    socket.on('send group message', function (group, sender, message, time) {
-        socket.in(group).emit('group message', { message, sender, time });
-        socket.emit('sender group message', { message, sender, time });
-    });
-    socket.on('leave group', function (group) {
-        socket.leave(group);
-    });
+    socket(io);
 });
--- server/sockets/index.js
+++ server/sockets/index.js
@@ -1,34 +1,38 @@
 module.exports = (io) => {
+    console.log('connecting to sockets...');
+    //socket configuration
     let onlineUsers = [];
-    
     io.on('connection', function (socket) {
         socket.on('user name', (user) => {
             const details = { profileName: user.profileName, sessionId: socket.id };
             onlineUsers.push(details);
             io.emit('connectedUsers', onlineUsers); //io.sockets.emit == io.emit
         });
-    
+
         socket.on('disconnect', function () {
             onlineUsers = onlineUsers.filter((user) => user.sessionId !== socket.id);
             io.emit('connectedUsers', onlineUsers);
+            // socket.leave(group);
         });
-    
+
         //personal chat
         socket.on('chatting', function (message, sender, receiver) {
             socket.to(receiver).emit('receiverPeer', message, socket.id, receiver);
             socket.emit('senderPeer', message, socket.id, receiver);
         });
-    
+
         //group chat
         socket.on('assign users', function (group) {
             socket.join(group);
         });
-        socket.on('send group message', function (group, sender, message) {
-            socket.in(group).emit('group message', message, sender);
-            socket.emit('sender group message', message);
-        });
         socket.on('leave group', function (group) {
             socket.leave(group);
         });
+        socket.on('send group message', function (group, senderId, senderName, message, createdAt) {
+            socket.in(group).emit('group message', { message, senderId, senderName }); //for users of joined in current session
+            socket.emit('sender group message', { message, senderId, senderName }); //for all users of the group
+        });
     });
+
+    return io;
 }
